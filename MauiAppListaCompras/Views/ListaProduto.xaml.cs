36944f1bf691d54cbc9983d621c0bd6caca9293f
duilersilva <duilerdd@gmail.com>
2025-03-16 17:06:12 -0300

    adicionado tarefa somar, grid com informações do produto

--- MauiAppListaCompras/Views/ListaProduto.xaml.cs
+++ MauiAppListaCompras/Views/ListaProduto.xaml.cs
@@ -1,11 +1,26 @@
+using MauiAppListaCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppListaCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
-	public ListaProduto()
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
+    public ListaProduto()
 	{
-		InitializeComponent();
-	}
+        InitializeComponent();
+
+        lst_produtos.ItemsSource = lista;
+    }
+
+    protected async override void OnAppearing()
+	{
+        List<Produto> tmp = await App.Db.GetAll();
+
+		tmp.ForEach(i => lista.Add(i));
+    }
+
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
@@ -16,6 +31,31 @@ public partial class ListaProduto : ContentPage
 		}catch (Exception ex)
 		{
 			DisplayAlert("Ops", ex.Message, "ok");
-				}
+		}
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		string q = e.NewTextValue;
+
+		lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+        double soma = lista.Sum(i => i.Total);
+
+        string mensagem = $"O Total: {soma:C}";
+
+        DisplayAlert("Total de Produtos", mensagem, "Ok");
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+
     }
 }
