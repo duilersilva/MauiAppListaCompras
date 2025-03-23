53d31cfa44e777869432b44e87f0bbe5b9cbeb52
duilersilva <duilerdd@gmail.com>
2025-03-23 16:59:04 -0300

    incluido referencia a model para incluir preco quantidade editar um produto ou adicionar um novo

--- MauiAppListaCompras/Models/Produto.cs
+++ MauiAppListaCompras/Models/Produto.cs
@@ -5,6 +5,8 @@ namespace MauiAppListaCompras.Models
     public class Produto
     {
         string _descricao;
+        double _quantidade;
+        double _preco;
 
         [PrimaryKey, AutoIncrement] 
         public int Id { get; set; }
@@ -19,8 +21,28 @@ namespace MauiAppListaCompras.Models
                 _descricao = value;
             }        
         }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public double Quantidade { 
+            get => _quantidade;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("A quantidade prescisa ser maior que zero");
+                }
+                _quantidade = value;
+            }
+        }
+        public double Preco { 
+            get => _preco;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("O preço prescisa ser maior que zero");
+                }
+                _preco = value;
+            }
+        }
         public double Total { get => Quantidade * Preco; }
     
 
